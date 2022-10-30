30db584ed6413598793767acd7900ee513ef2e9f
isilla <neverovau5@gmail.com>
2022-10-30 11:14:41 +0500

    First commit

---
+++ CommonOne.CLI/Program.cs
@@ -0,0 +1,5 @@
+﻿using System.Text;
+
+Console.OutputEncoding = Encoding.Unicode;
+Console.WriteLine("Почему я хочу быть разработчиком?\nДа хрен его знает.\nНичего интереснее разработки я не смог найти.");
+Console.ReadLine();
---
+++ CommonOne.Models/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace CommonOne.Models;
+public class Class1
+{
+
+}
